11a3afd4d7ab00f555fbd3bae9ef52e6e188e8d0
RaviShankar5777 <raviishankar77773@gmail.com>
2023-12-14 19:20:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+
+
+const images = document.querySelector(".images");
+const burger = document.querySelector(".burger");
+
+const firstImg = document.querySelector(".img1");
+const secondImg = document.querySelector(".img2");
+const thirdImg = document.querySelector(".img3");
+const fourthImg = document.querySelector(".img4");
+const fifthImg = document.querySelector(".img5");
+const sixthImg = document.querySelector(".img6");
+const seventhImg = document.querySelector(".img7");
+const eighthImg = document.querySelector(".img8");
+const ninthImg = document.querySelector(".img9");
+const tenthImg = document.querySelector(".img10");
+
+function image1Function() {
+  firstImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function burgerFunction() {
+  firstImg.style.display = "none";
+  secondImg.style.display = "none";
+  thirdImg.style.display = "none";
+  fourthImg.style.display = "none";
+  fifthImg.style.display = "none";
+  sixthImg.style.display = "none";
+  seventhImg.style.display = "none";
+  eighthImg.style.display = "none";
+  ninthImg.style.display = "none";
+  tenthImg.style.display = "none";
+  images.style.opacity = "1";
+  burger.style.display = "none";
+}
+
+function image2Function() {
+  secondImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function image3Function() {
+  thirdImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function image4Function() {
+  fourthImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function image5Function() {
+  fifthImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function image6Function() {
+  sixthImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function image7Function() {
+  seventhImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function image8Function() {
+  eighthImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function image9Function() {
+  ninthImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
+
+function image10Function() {
+  tenthImg.style.display = "block";
+  images.style.opacity = "0.2";
+  burger.style.display = "block";
+}
